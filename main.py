16529c9cb34bdd71e9fe30a4c7ff11e84e134452
3B HADY <alhdry420@gmail.com>
2024-08-03 21:01:42 +0300

    add saved texts file and set up greating messages

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import telebot
 import matplotlib
+from saved_texts import *
+from manageJsonFiles import *
+from telebot.types import Message, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+token = ""
+bot = telebot.TeleBot (token)
 
 def converting_time (value, timefrom, timeto):
     "from the smallest to the biggest such as 3600 second = 1 hour"
@@ -23,3 +28,21 @@ def converting_time (value, timefrom, timeto):
     for time in lst :
         result = result / times [time]
     return result
+
+#! start chating method
+@bot.message_handler (commands= ["start"])
+def start (message : Message):
+    customers_data = get_json_data ("customers")
+    id_list = customers_data ["selling_processes"]
+    name = message.from_user.first_name
+    chat_id = message.chat.id
+    if chat_id in id_list:
+        return bot.send_message (chat_id, second_start_message (name))
+    customers_data = add_obj2list ("customers", "selling_processes", str (message.chat.id))
+    updata_data ("customers", customers_data)
+    bot.send_message (chat_id, first_start_message (name))
+    
+#! programmer informing message
+#! about the bot message
+#! sotre main menu buttons
+bot.infinity_polling ()
---
+++ saved_texts.py
@@ -0,0 +1,62 @@
+bot_name = "تيلو"
+store_name = "3B HADY Store"
+first_start_message = lambda name : f"مرحبا {name} انا {bot_name} تاجرك لمتجر {store_name}"
+second_start_message = lambda name : f"مرحبا بك مرة اخرى يا {name} في متجرك {store_name}"
+about_bot = lambda chats_num, succeed_process_number, users_rate, users_comments : f"""
+ع̲ن البوت
+====================
+--- معلومات ---
+الإسم : {bot_name}
+بدأت برمجته في : 8/3/2024
+تم اصداره في : 00000000
+
+====================
+--- إحصائيات ---
+عدد المحادثات : {chats_num}
+عمليات الشراء الناجحة من خلال البوت : {succeed_process_number}
+تقييم المستخدمين : {users_rate}
+
+====================
+--- تقييم البوت ---
+
+{users_comments}
+
+====================
+--- المبرمج ---
+المبرمج : /dev
+
+====================
+"""
+
+programmer_text = """
+about progarmmer
+====================
+--- Information ---
+♜ Name : Ahmed Abd Elhady
+♜ NickName1 : 3B HADY
+♜ NickName2 : El7adry
+♜ Age : 16 -> 17
+
+====================
+--- Edu and CS ---
+♜ Edu stage : 11th class
+    ( 2nd secondary school )
+
+♜ CS details :
+  - programming languages :
+    * python (very good)
+    * JavaScript (under middle)
+  - problem solving : 
+    * level 6 in codewars.com
+    * good in normal problems
+  - hardware knowlege : 
+    * middle level
+
+====================
+--- Media ---
+♜ contact : 01096940855
+♜ email : alhdry420@gmail.com
+♜ another media links :
+ ⇓ ⇓ ⇓ ⇓ ⇓ ⇓ ⇓ ⇓ 
+"""
+
